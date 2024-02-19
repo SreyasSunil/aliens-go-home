1f11b1ab717bf2a7fdfd3e74034d3badbd03645a
Sreyas <sreyassunil5@gmail.com>
2024-02-19 12:18:04 +0400

    ADDED:: Redux states, Components and functionalities

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-import App from './App';
+import Game from './containers/Game';
+import reducer from './reducers';
 import reportWebVitals from './reportWebVitals';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Game />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
